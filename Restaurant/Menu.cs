4079c47b558b867d6820d916bd1119e722596e37
chiefD <105441682+AltaafDoekhie@users.noreply.github.com>
2023-11-20 13:33:50 +0100

    Update Menu.cs

--- Restaurant/Menu.cs
+++ Restaurant/Menu.cs
@@ -9,7 +9,7 @@ public class Menu
         Console.WriteLine("-----------------------------");
         Console.WriteLine("Food menu:");
         Console.WriteLine("-----------------------------");
-        string json = File.ReadAllText("Menu_Food.json");
+        string json = File.ReadAllText("C:\\Users\\altaa\\OneDrive\\Documents\\Restuarant pro\\Restuarant pro\\Menu_Food.json");
         List<Food> existingFoods = JsonConvert.DeserializeObject<List<Food>>(json);
         foreach (Food food_item in existingFoods)
         {
@@ -24,11 +24,11 @@ public class Menu
         Console.WriteLine("-----------------------------");
         Console.WriteLine("Drink menu:");
         Console.WriteLine("-----------------------------");
-        string json_ = File.ReadAllText("Menu_Drink.json");
+        string json_ = File.ReadAllText("C:\\Users\\altaa\\OneDrive\\Documents\\Restuarant pro\\Restuarant pro\\Menu_Drink.json");
         List<Drink> existingdrinks = JsonConvert.DeserializeObject<List<Drink>>(json_);
         foreach (Drink drink_item in existingdrinks)
         {
-            Console.WriteLine($"Dish: {drink_item.Name}, Price: {drink_item.Price}");
+            Console.WriteLine($"Drink: {drink_item.Name}, Price: {drink_item.Price}");
         }
         Console.WriteLine("-----------------------------");
     }
@@ -36,7 +36,7 @@ public class Menu
     public void Add_food(Food Food_item)
     {
         // Deserialize the existing data from the JSON file
-        string json = File.ReadAllText("Menu_Food.json");
+        string json = File.ReadAllText("C:\\Users\\altaa\\OneDrive\\Documents\\Restuarant pro\\Restuarant pro\\Menu_Food.json");
         List<Food> existingFoods = JsonConvert.DeserializeObject<List<Food>>(json);
 
         // Add the new food item to the existing list
@@ -44,13 +44,13 @@ public class Menu
 
         // Serialize and write the updated list back to the JSON file
         string updatedJson = JsonConvert.SerializeObject(existingFoods, Formatting.Indented);
-        File.WriteAllText("Menu_Food.json", updatedJson);
+        File.WriteAllText("C:\\Users\\altaa\\OneDrive\\Documents\\Restuarant pro\\Restuarant pro\\Menu_Food.json", updatedJson);
     }
 
     public void Add_drink(Drink Drink_item)
     {
         // Deserialize the existing data from the JSON file
-        string json = File.ReadAllText("Menu_Drink.json");
+        string json = File.ReadAllText("C:\\Users\\altaa\\OneDrive\\Documents\\Restuarant pro\\Restuarant pro\\Menu_Drink.json");
         List<Drink> existingdrinks = JsonConvert.DeserializeObject<List<Drink>>(json);
 
         // Add the new food item to the existing list
@@ -58,6 +58,62 @@ public class Menu
 
         // Serialize and write the updated list back to the JSON file
         string updatedJson = JsonConvert.SerializeObject(existingdrinks, Formatting.Indented);
-        File.WriteAllText("Menu_Drink.json", updatedJson);
+        File.WriteAllText("C:\\Users\\altaa\\OneDrive\\Documents\\Restuarant pro\\Restuarant pro\\Menu_Drink.json", updatedJson);
+    }
+
+    public void Delete_food()
+    {
+        Console.WriteLine("Name of the dish that you want to delete: ");
+        string foodName = Console.ReadLine();
+
+
+        // Deserialize the existing data from the JSON file
+        string json = File.ReadAllText("C:\\Users\\altaa\\OneDrive\\Documents\\Restuarant pro\\Restuarant pro\\Menu_Food.json");
+        List<Food> existingFoods = JsonConvert.DeserializeObject<List<Food>>(json);
+
+        // Find and remove the food item by its name
+        Food foodToRemove = existingFoods.FirstOrDefault(food => food.Name == foodName);
+        if (foodToRemove != null)
+        {
+            existingFoods.Remove(foodToRemove);
+
+            // Serialize and write the updated list back to the JSON file
+            string updatedJson = JsonConvert.SerializeObject(existingFoods, Formatting.Indented);
+            File.WriteAllText("C:\\Users\\altaa\\OneDrive\\Documents\\Restuarant pro\\Restuarant pro\\Menu_Food.json", updatedJson);
+
+            Console.WriteLine($"Food item '{foodName}' has been deleted from the menu.");
+        }
+        else
+        {
+            Console.WriteLine($"Food item '{foodName}' was not found in the menu.");
+        }
+    }
+
+    public void Delete_drink()
+    {
+
+        Console.WriteLine("Name of the drink that you want to delete: ");
+
+        string drinkName = Console.ReadLine();
+        // Deserialize the existing data from the JSON file
+        string json = File.ReadAllText("C:\\Users\\altaa\\OneDrive\\Documents\\Restuarant pro\\Restuarant pro\\Menu_Drink.json");
+        List <Drink> existingDrinks = JsonConvert.DeserializeObject<List<Drink>>(json);
+
+        // Find and remove the drink item by its name
+        Drink drinkToRemove = existingDrinks.FirstOrDefault(drink => drink.Name == drinkName);
+        if (drinkToRemove != null)
+        {
+            existingDrinks.Remove(drinkToRemove);
+
+            // Serialize and write the updated list back to the JSON file
+            string updatedJson = JsonConvert.SerializeObject(existingDrinks, Formatting.Indented);
+            File.WriteAllText("Menu_Drink.json", updatedJson);
+
+            Console.WriteLine($"Drink item '{drinkName}' has been deleted from the menu.");
+        }
+        else
+        {
+            Console.WriteLine($"Drink item '{drinkName}' was not found in the menu.");
+        }
     }
 }
