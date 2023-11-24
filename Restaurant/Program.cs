a1715d2b9b693b89464a262cfd30f012ab84e4ef
Jerrel <144031624+jer-jerrel@users.noreply.github.com>
2023-11-24 15:44:12 +0100

    Update Program.cs

--- Restaurant/Program.cs
+++ Restaurant/Program.cs
@@ -1,11 +1,10 @@
-﻿using System.Reflection.Metadata.Ecma335;
-using System.Globalization;
-using System;
+using Newtonsoft.Json;
+using System.Text.Json;
+using System.Text.RegularExpressions;
 
-public class Program
+public static class Program
 {
     public static User currentUser = null;
-    public static Menu menu = new Menu();
     public static List<Table> tables = new List<Table>()
     {
         new Table(1, 2), new Table(2, 2), new Table(3, 2), new Table(4, 2), new Table(5, 2), new Table(6, 2), new Table(7, 2), new Table(16, 6),
@@ -22,303 +21,103 @@ public class Program
             Console.WriteLine("|                                |");
             Console.WriteLine("+--------------------------------+");
             Console.WriteLine("| Options:                       |");
-            Console.WriteLine("| 1. Make Reservation            |");
+            Console.WriteLine("| 1. Login or Signup             |");
             Console.WriteLine("| 2. Menu                        |");
-            Console.WriteLine("| 3. Location                    |");
-            Console.WriteLine("| 4. login                       |");
-            Console.WriteLine("| 5. Exit                        |");
-            if (currentUser != null && currentUser.Admin)
-            {
-                Console.WriteLine("| 6. Admin Menu                  |");
-            }
+            Console.WriteLine("| 3. Restaurant Information      |");
+            Console.WriteLine("| 4. Exit                        |");
             Console.WriteLine("+--------------------------------+");
-            if (currentUser != null && currentUser.Admin)
-            {
-                Console.Write("Please select an option (1/2/3/4/5/6): ");
-            }
-            else
-            {
-                Console.Write("Please select an option (1/2/3/4/5): ");
-            }
-            string userInput = Console.ReadLine();
-
-            switch (userInput)
+            Console.WriteLine("Please pick an option (1/2/3/4):  ");
+            string userinput = Console.ReadLine();
+            switch (userinput)
             {
                 case "1":
-                    if (currentUser is null)// currentUser is null
+                    Login_or_Signup();
+                    Console.WriteLine($"{currentUser.GetType()}");
+                    bool Loggedin = true;
+                    while (Loggedin)
                     {
-                        Console.WriteLine("Please log or sign up in first to make a reservation.");
-                        login_or_Signup();
-                        Console.ReadKey();
-                        Console.Clear();
-                    }
-                    else
-                    {
-                        Console.WriteLine("1. Make Reservation");
-                        Console.WriteLine("2. Remove Reservation");
-                        Console.Write("Please select an option (1/2): ");
-                        string reservationOption = Console.ReadLine();
-                        switch (reservationOption)
-                        {
-                            case "1":
-                                RestaurantMap.DisplayMap();
-                                bool Incomplete = true;
-                                while (Incomplete)
-                                {
-                                    Console.Write("Enter date and time (yyyy-MM-dd HH:mm) or exit: ");
-                                    string reservationUserInput = Console.ReadLine() + ":00";
-                                    string format = "yyyy-MM-dd HH:mm:ss";
-                                    if (reservationOption.ToUpper() == "EXIT")
-                                    {
-                                        Main();
-                                    }
-                                    if (DateTime.TryParseExact(reservationUserInput, format, null, DateTimeStyles.None, out DateTime result))
-                                    {
-                                        Console.WriteLine("DateTime using DateTime.TryParseExact: " + result);
-                                        Tuple<DateTime, DateTime> reservation_time = new Tuple<DateTime, DateTime>(result, result.AddHours(1));
-                                        Reserve.MakingReservation(currentUser.Name, currentUser.Email, tables, reservation_time);
-                                        Incomplete = false;
-                                    }
-                                    else
-                                    {
-                                        Console.WriteLine("Invalid date format");
-                                    }
-                                }
-                                Console.ReadKey();
-                                Console.Clear();
-                                break;
-                            case "2":
-                                Reserve.CancelReservation(currentUser);
-                                Console.ReadKey();
-                                Console.Clear();
-                                break;
-                            default:
-                                Console.WriteLine("Invalid input. Please select a valid option.");
-                                break;
-                                        
-                        }
+                        currentUser.UserMenu();
+                        if (currentUser.UserInput(currentUser, tables) is false)
+                            Loggedin = false;
                     }
                     break;
-
                 case "2":
-                    showMenu();
+                    Menu.Display_menu();
                     Console.ReadKey();
                     Console.Clear();
                     break;
                 case "3":
-                    restInfo();
+                    Information.ContactInfo();
                     Console.ReadKey();
                     Console.Clear();
                     break;
                 case "4":
-                    login_or_Signup();
-                    Console.ReadKey();
-                    Console.Clear();
+                    Console.WriteLine("Exiting the app. Goodbye!");
+                    Environment.Exit(0);
                     break;
-                case "5":
-                    ExitGame();
-                    break;
-                case "6":
-                    if (currentUser.Admin)
-                    {
-                        AdminMenu();
-                        break;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Invalid input. Please select a valid option.");
-                        break;
-                    }
                 default:
                     Console.WriteLine("Invalid input. Please select a valid option.");
                     break;
             }
         }
-
-        static void login_or_Signup()
-        {
-            Console.WriteLine("Do you want to LOGIN(1) or Sign up(2) ");
-            string user_answer = Console.ReadLine().ToLower();
-
-            if (user_answer == "1")
-            {
-                Login userLogin = new Login();
-                // this causes error because the PromptForLogin method does not return a User.
-                currentUser = userLogin.PromptForLogin();
-                if (currentUser == null)
-                {
-                    Console.WriteLine("Please try again or sign up.");
-                }
-
-                string email = userLogin.Email;
-                string password = userLogin.Password;
-
-                Console.WriteLine("Logging in with email: " + email);
-            }
-            else if (user_answer == "2")
-            {
-                Signup usersignup = new Signup();
-                Console.WriteLine("Please enter your name: ");
-                string name = Console.ReadLine();
-                Console.WriteLine("Please enter your email: ");
-                string email = Console.ReadLine();
-                Console.WriteLine("Please enter a password that needs to be 8 characters.");
-                string password = Console.ReadLine();
-
-                // Call the SignUp method to validate and save user information
-                currentUser = usersignup.SignUp(name, email, password);
-            }
-            else
-            {
-                Console.WriteLine("Invalid choice. Please select 'Login' or 'Sign up'.");
-            }
-        }
-
-        static void restInfo()
-        {
-            Console.WriteLine("+----------------------------------------------+");
-            Console.WriteLine("| Jake’s restaurant information:               |");
-            Console.WriteLine("| Location: Wijnhaven 107, 3011 WN in Rotterdam|");
-            Console.WriteLine("| Phone: (123) 456-7890                        |");
-            Console.WriteLine("| Email: jakes@example.com                     |");
-            Console.WriteLine("+----------------------------------------------+");
-        }
-
-        static void showMenu()
-        {
-            Console.WriteLine("\nMenu:");
-            menu.Display_menu();
-        }
-
-        static void ExitGame()
-        {
-            Console.WriteLine("Exiting the app. Goodbye!");
-            Environment.Exit(0);
-        }
-
-        static void Add_Item_Menu()
-        {
-            Console.WriteLine("What would you like to add?\n1: Food\n2: Drink");
-            string userInput = Console.ReadLine();
-            switch (userInput)
-            {
-                case "1":
-                    bool Dish_vegan = true;
-                    string action;
-                    bool completed = true;
-                    Console.WriteLine("What is the name of the dish?");
-                    string Dish_name = Console.ReadLine();
-                    Console.WriteLine("What is the price of the dish?");
-                    double Dish_price = Convert.ToDouble(Console.ReadLine());
-
-                    while (completed)
-                    {
-                        Console.WriteLine("Is the dish vegan? (Y/N?)");
-                        action = Console.ReadLine().ToUpper();
-                        if (action == "Y")
-                        {
-                            Dish_vegan = true;
-                            completed = false;
-                        }
-                        else if (action == "N")
-                        {
-                            Dish_vegan = false;
-                            completed = false;
-                        }
-                        else
-                        {
-                            Console.WriteLine("Invalid input");
-                        }
-                    }
-                    Food new_dish = new Food(Dish_name, Dish_price, Dish_vegan);
-                    Console.WriteLine("The item has been added to the menu.");
-                    menu.Add_food(new_dish);
-                    break;
-                case "2":
-                    Console.WriteLine("What is the name of the drink?");
-                    string drink_name = Console.ReadLine();
-                    Console.WriteLine("What is the price of the drink?");
-                    double drink_price = Convert.ToDouble(Console.ReadLine());
-                    Drink new_drink = new Drink(drink_name, drink_price);
-                    menu.Add_drink(new_drink);
-                    Console.WriteLine("The item has been added to the menu.");
-                    break;
-            }
-        }
-        static void Remove_Item_Menu()
-        {
-            Console.WriteLine("What would you like to remove?\n1: Food item\n2: Drink item");
-            string userInput = Console.ReadLine();
-            switch (userInput)
-            {
-                case "1":
-                    menu.Delete_food();
-                    break;
-                case "2":
-                    menu.Delete_drink();
-                    break;
-            }
-        }
-        static void AdminMenu()
-        {
-            while (true)
-            {
-                Console.Clear();
-                Console.WriteLine("+--------------------------------+");
-                Console.WriteLine("|                                |");
-                Console.WriteLine("|  Welcome Admin                 |");
-                Console.WriteLine("|                                |");
-                Console.WriteLine("+--------------------------------+");
-                Console.WriteLine("| Options:                       |");
-                Console.WriteLine("| 1. Add item to Menu            |");
-                Console.WriteLine("| 2. Remove item from Menu       |");
-                Console.WriteLine("| 3. Create Admin                |");
-                Console.WriteLine("| 4.                             |");
-                Console.WriteLine("| 5. return                      |");
-                Console.WriteLine("+--------------------------------+");
-                Console.Write("Please select an option (1/2/3/4/5): ");
-                string AdminInput = Console.ReadLine();
-                switch (AdminInput)
-                {
-                    case "1":
-                        Add_Item_Menu();
-                        Console.ReadKey();
-                        break;
-                    case "2":
-                        Remove_Item_Menu();
-                        Console.ReadKey();
-                        break;
-                    case "3":
-                        // Check if user is a Superadmin
-                        if (currentUser.Superadmin)
-                        {
-                            Signup usersignup = new Signup();
-                            Console.WriteLine("What is the name of the Admin?");
-                            string Name = Console.ReadLine();
-                            Console.WriteLine("What is the Email of the admin?");
-                            string Email = Console.ReadLine();
-                            Console.WriteLine("What is the password of the admin");
-                            string Password = Console.ReadLine();
-                            usersignup.SignUp(Name, Email, Password, true);
-                            Console.ReadKey();
-                            break;
-                        }
-                        else
-                        {
-                            Console.WriteLine("User does not have the privalages to proceed.");
-                            Console.ReadKey();
-                            break;
-                        }
-
-                    case "5":
-                        Main();
-                        break;
-                    default:
-                        Console.WriteLine("Invalid input. Please select a valid option.");
-                        break;
-                }
-            }
-        }
     }
 
+    public static void Login_or_Signup()
+    {
+        Console.WriteLine("Do you want to LOGIN(1) or Sign up(2) ");
+        string user_answer = Console.ReadLine().ToLower();
+
+        if (user_answer == "1")
+        {
+            Login userLogin = new Login();
+            // this causes error because the PromptForLogin method does not return a User.
+            currentUser = userLogin.PromptForLogin();
+            if (currentUser == null)
+            {
+                Console.WriteLine("Please try again or sign up.");
+            }
+
+            string email = currentUser.Email;
+
+            Console.WriteLine("Logging in with email: " + email);
+        }
+        else if (user_answer == "2")
+        {
+            Signup usersignup = new Signup();
+            Console.WriteLine("Please enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Please enter your email: ");
+            string email = Console.ReadLine();
+            Console.WriteLine("Please enter your password: ");
+            string password = Console.ReadLine();
+
+            // Call the SignUp method to validate and save user information
+            currentUser = usersignup.SignUp(name, email, password, false);
+        }
+        Console.ReadKey();
+        Console.Clear();
+    }
+    //CODE BELOW TO HARDCODE A SUPERADMIN
+    //==================================
+
+    /*string filePath = "C:\\Users\\jerre\\OneDrive\\Bureaublad\\projectbb\\projectbb\\User_info.json";
+
+// Read existing user data from the file
+List<User> users = JsonConvert.DeserializeObject<List<User>>(File.ReadAllText(filePath), new JsonSerializerSettings
+{
+    TypeNameHandling = TypeNameHandling.All
+});
+
+// Add the hardcoded superadmin to the list
+SuperAdmin superAdmin = new SuperAdmin("superadmin", "superadmin@example.com", "superadmin");
+users.Add(superAdmin);
+
+// Serialize the updated list of users with type information
+string jsonString = JsonConvert.SerializeObject(users, Formatting.Indented, new JsonSerializerSettings
+{
+    TypeNameHandling = TypeNameHandling.All
+});
+
+// Write the updated user data back to the file
+//File.WriteAllText(filePath, jsonString);*/
 }
