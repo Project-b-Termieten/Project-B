d0b4a6d816ee3093b93bbb5b0bc2c90908353150
Jerrel <144031624+jer-jerrel@users.noreply.github.com>
2023-11-24 15:45:53 +0100

    Update User.cs
    nu een base class van admin en superadmin met virtual methods

--- Restaurant/User.cs
+++ Restaurant/User.cs
@@ -1,40 +1,76 @@
 using Newtonsoft.Json;
 
+
+
 public class User
 {
     public string Name { get; set; }
-    public string Email { get; set; }
+    public string Email { get; set; }  // Add a public setter
     public string Password { get; set; }
-
-    [JsonProperty("Admin")]
-    public bool Admin { get; set; }
-
-    [JsonProperty("Superadmin")]
-    public bool Superadmin { get; set; }
-
-    // Parameterless constructor
-    public User()
-    {
-        // Initialize default values if needed
-        Admin = false;
-        Superadmin = false;
-    }
+    public bool IsAdmin { get; set; }
+    public bool IsSuperAdmin { get; set; }
 
     public User(string name, string email, string password)
     {
         Name = name;
         Email = email;
         Password = password;
-        Admin = false;
-        Superadmin = false;
+        IsAdmin = false;
+        IsSuperAdmin = false;
     }
 
-    public User(string name, string email, string password, bool admin)
+
+public virtual void UserMenu()
     {
-        Name = name;
-        Email = email;
-        Password = password;
-        Admin = admin;
-        Superadmin = false;
+        Console.WriteLine("+--------------------------------+");
+        Console.WriteLine("|                                |");
+        Console.WriteLine("|  Welcome to Jake’s restaurant! |");
+        Console.WriteLine("|                                |");
+        Console.WriteLine("+--------------------------------+");
+        Console.WriteLine("| Options:                       |");
+        Console.WriteLine("| 1. Make Reservation            |");
+        Console.WriteLine("| 2. Menu                        |");
+        Console.WriteLine("| 3. Restaurant Information      |");
+        Console.WriteLine("| 4. Logout                      |");
+        Console.WriteLine("| 5. Exit                        |");
+        Console.WriteLine("+--------------------------------+");
+        Console.WriteLine("Please pick an option (1/2/3/4/5):");
+    }
+
+    public virtual bool UserInput(User currentUser, List<Table> tables)
+    {
+        string userInput = Console.ReadLine();
+
+        switch (userInput)
+        {
+            case "1":
+                Information.DisplayMap();
+                Reserve.MakingReservation(currentUser.Name, currentUser.Email, tables);
+                Console.ReadKey();
+                Console.Clear();
+                return true;
+            case "2":
+                Menu.Display_menu();
+                Console.ReadKey();
+                Console.Clear();
+                return true;
+            case "3":
+                Information.ContactInfo();
+                Console.ReadKey();
+                Console.Clear();
+                return true;
+            case "4":
+                Console.WriteLine("Log out successful.");
+                Console.ReadKey();
+                Console.Clear();
+                return false;
+            case "5":
+                Console.WriteLine("Exiting the app. Goodbye!");
+                Environment.Exit(0);
+                return true;
+            default:
+                Console.WriteLine("Invalid input. Please select a valid option.");
+                return true;
+        }
     }
 }
