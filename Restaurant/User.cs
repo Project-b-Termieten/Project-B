3f64b0f554a5f0b65f10102e3a9e7735e2b934de
Aidan <143912273+AidanYappington@users.noreply.github.com>
2023-11-26 02:57:28 +0100

    Update User.cs
    Altaafs methode toegevoegd in menu, user kan nu reservering method selecteren

--- Restaurant/User.cs
+++ Restaurant/User.cs
@@ -10,6 +10,10 @@ public class User
     public bool IsAdmin { get; set; }
     public bool IsSuperAdmin { get; set; }
 
+    public bool HasReserved = false;
+
+    public Tuple<DateTime, DateTime> Time { get; set; }
+
     public User(string name, string email, string password)
     {
         Name = name;
@@ -20,21 +24,43 @@ public class User
     }
 
 
-public virtual void UserMenu()
+    public virtual void UserMenu()
     {
-        Console.WriteLine("+--------------------------------+");
-        Console.WriteLine("|                                |");
-        Console.WriteLine("|  Welcome to Jake’s restaurant! |");
-        Console.WriteLine("|                                |");
-        Console.WriteLine("+--------------------------------+");
-        Console.WriteLine("| Options:                       |");
-        Console.WriteLine("| 1. Make Reservation            |");
-        Console.WriteLine("| 2. Menu                        |");
-        Console.WriteLine("| 3. Restaurant Information      |");
-        Console.WriteLine("| 4. Logout                      |");
-        Console.WriteLine("| 5. Exit                        |");
-        Console.WriteLine("+--------------------------------+");
-        Console.WriteLine("Please pick an option (1/2/3/4/5):");
+        if (HasReserved == false)
+        {
+            Console.WriteLine("+--------------------------------+");
+            Console.WriteLine("|                                |");
+            Console.WriteLine("|  Welcome to Jake’s restaurant! |");
+            Console.WriteLine("|                                |");
+            Console.WriteLine("+--------------------------------+");
+            Console.WriteLine("| Options:                       |");
+            Console.WriteLine("| 1. Make Reservation            |");
+            Console.WriteLine("| 2. Menu                        |");
+            Console.WriteLine("| 3. Restaurant Information      |");
+            Console.WriteLine("| 4. Logout                      |");
+            Console.WriteLine("| 5. Exit                        |");
+            Console.WriteLine("+--------------------------------+");
+            Console.WriteLine("Please pick an option (1/2/3/4/5):");
+        }
+
+        else
+        {
+            Console.WriteLine("+--------------------------------+");
+            Console.WriteLine("|                                |");
+            Console.WriteLine("|  Welcome to Jake’s restaurant! |");
+            Console.WriteLine("|                                |");
+            Console.WriteLine("+--------------------------------+");
+            Console.WriteLine("| Options:                       |");
+            Console.WriteLine("| 1. Make Reservation            |");
+            Console.WriteLine("| 2. Menu                        |");
+            Console.WriteLine("| 3. Restaurant Information      |");
+            Console.WriteLine("| 4. Logout                      |");
+            Console.WriteLine("| 5. Exit                        |");
+            Console.WriteLine("|                                |");
+            Console.WriteLine("| 6. Cancel Reservation          |");
+            Console.WriteLine("+--------------------------------+");
+            Console.WriteLine("Please pick an option (1/2/3/4/5):");
+        }
     }
 
     public virtual bool UserInput(User currentUser, List<Table> tables)
@@ -45,7 +71,8 @@ public virtual void UserMenu()
         {
             case "1":
                 Information.DisplayMap();
-                Reserve.MakingReservation(currentUser.Name, currentUser.Email, tables, currentUser.Time); //26 oktober ->currentUser.Time);
+                Reserve.MakingReservation(currentUser.Name, currentUser.Email, tables, currentUser.Time); //dit gechanged
+                HasReserved = true;
                 Console.ReadKey();
                 Console.Clear();
                 return true;
@@ -66,6 +93,10 @@ public virtual void UserMenu()
                 return false;
             case "5":
                 Console.WriteLine("Exiting the app. Goodbye!");
+                Environment.Exit(0);
+                return true;
+            case "6":
+                Reserve.CancelReservation(currentUser);
                 Environment.Exit(0);
                 return true;
             default:
