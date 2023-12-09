690b83e3e72d19291d1f7a39c8bdc09daef82116
Aidan <143912273+AidanYappington@users.noreply.github.com>
2023-12-09 03:55:12 +0100

    Create JsonPaths.cs
    not yet finished class

---
+++ Restaurant/JsonPaths.cs
@@ -0,0 +1,23 @@
+    /*
+    --- Nog niet in gebruik---
+    public static class JsonPaths
+    {
+        public static class Users
+        {
+            public const string jasonusers = @"C:\Users\aidan\OneDrive\Documenten\c# docs\RestaurantAltaaf\RestaurantAltaaf\User_info.json";
+        }
+
+        public static class ReservationPaths
+        {
+            public const string jasonreservations = @"C:\Users\aidan\OneDrive\Documenten\c# docs\RestaurantAltaaf\RestaurantAltaaf\User_info.json";
+        }
+
+         public static class MenuItems
+        {
+            public const string jasondrinktlean = @"C:\Users\aidan\OneDrive\Documenten\c# docs\RestaurantAltaaf\RestaurantAltaaf\User_info.json";
+            public const string jasonfood = @"C:\Users\aidan\OneDrive\Documenten\c# docs\RestaurantAltaaf\RestaurantAltaaf\User_info.json";
+
+        }
+    }
+    */
+
