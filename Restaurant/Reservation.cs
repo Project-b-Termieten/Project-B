317735061ca19560f4cc72bc377ec6c9f6d5a7fb
chiefD <105441682+AltaafDoekhie@users.noreply.github.com>
2023-12-10 01:40:30 +0100

    Update Reservation.cs

--- Restaurant/Reservation.cs
+++ Restaurant/Reservation.cs
@@ -1,9 +1,13 @@
+using System.Dynamic;
+
 public class Reservation
 {
     public string Name { get; }
     public string Email { get; }
     public int Amount { get; } = 0;
     public Table Table { get; } = null;
+
+    public Tuple<DateTime, DateTime> Time = new Tuple<DateTime, DateTime>(new DateTime(), new DateTime());
 
     public Reservation(string name, string email, int amount, Table table)
     {
