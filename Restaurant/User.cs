1fa3ac860fa77001580abb5a042ee77059869490
chiefD <105441682+AltaafDoekhie@users.noreply.github.com>
2023-12-10 01:36:36 +0100

    Update User.cs

--- Restaurant/User.cs
+++ Restaurant/User.cs
@@ -1,40 +1,148 @@
 using Newtonsoft.Json;
+using System.Globalization;
 
-public class User
+
+public class User : IUserOperations
 {
     public string Name { get; set; }
-    public string Email { get; set; }
+    public string Email { get; set; }  // Add a public setter
     public string Password { get; set; }
+    public bool IsAdmin { get; set; }
+    public bool IsSuperAdmin { get; set; }
+    [JsonIgnore]
 
-    [JsonProperty("Admin")]
-    public bool Admin { get; set; }
-
-    [JsonProperty("Superadmin")]
-    public bool Superadmin { get; set; }
-
-    // Parameterless constructor
-    public User()
-    {
-        // Initialize default values if needed
-        Admin = false;
-        Superadmin = false;
-    }
+    public bool HasReserved { get; set; } = false;
+    [JsonIgnore]
+    public Tuple<DateTime, DateTime> Time { get; set; }
 
     public User(string name, string email, string password)
     {
         Name = name;
         Email = email;
         Password = password;
-        Admin = false;
-        Superadmin = false;
+        IsAdmin = false;
+        IsSuperAdmin = false;
     }
 
-    public User(string name, string email, string password, bool admin)
+
+    public virtual void UserMenu()
     {
-        Name = name;
-        Email = email;
-        Password = password;
-        Admin = admin;
-        Superadmin = false;
+        Console.WriteLine("+--------------------------------+");
+        Console.WriteLine("|                                |");
+        Console.WriteLine("|  Welcome to Jake’s restaurant! |");
+        Console.WriteLine("|                                |");
+        Console.WriteLine("+--------------------------------+");
+        Console.WriteLine("| Options:                       |");
+        Console.WriteLine("| 1. Reservation                 |");
+        Console.WriteLine("| 2. Menu                        |");
+        Console.WriteLine("| 3. Restaurant Information      |");
+        Console.WriteLine("| 4. Logout                      |");
+        Console.WriteLine("| 5. Exit                        |");
+        Console.WriteLine("| 6. Place Order                 |");
+        Console.WriteLine("+--------------------------------+");
+    }
+
+    public virtual bool UserInput(User currentUser, List<Table> tables)
+    {
+        string userInput = Console.ReadLine();
+
+        switch (userInput)
+        {
+            case "1":
+                Console.WriteLine("(1.) Make Reservation");
+                Console.WriteLine("(2.) Cancel Reservation");
+                string reservationInput = Console.ReadLine();
+                switch (reservationInput)
+                {
+                    case "1":
+                        Information.DisplayMap();
+                        bool Incomplete = true;
+                        DateTime selectedDateTime = DateTime.MinValue;
+
+                        while (Incomplete)
+                        {
+                            // Get the date from the user
+                            Console.Write("Enter date (yyyy-MM-dd): ");
+                            string dateString = Console.ReadLine();
+
+                            // Parse date string
+                            if (DateTime.TryParseExact(dateString, "yyyy-MM-dd", null, System.Globalization.DateTimeStyles.None, out selectedDateTime))
+                            {
+                                // Show reservations for the selected day
+                                Reserve.ShowReservationsForDay(selectedDateTime);
+
+                                // Get the time from the user
+                                Console.Write("Enter time (HH:mm): ");
+                                string timeString = Console.ReadLine();
+
+                                // Parse time string
+                                if (DateTime.TryParseExact(timeString, "HH:mm", null, System.Globalization.DateTimeStyles.None, out DateTime selectedTime))
+                                {
+                                    // Combine date and time
+                                    selectedDateTime = selectedDateTime.AddHours(selectedTime.Hour).AddMinutes(selectedTime.Minute);
+
+                                    // Display the selected date and time
+                                    Console.WriteLine($"Selected Date and Time: {selectedDateTime}");
+                                    Tuple<DateTime, DateTime> reservation_time = new Tuple<DateTime, DateTime>(selectedDateTime, selectedDateTime.AddHours(1));
+                                    Reserve.MakingReservation(currentUser.Name, currentUser.Email, tables, reservation_time);
+                                    Incomplete = false;
+                                    HasReserved = true;
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Invalid time format");
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("Invalid date format");
+                            }
+                        }
+                        break;
+
+                    case "2":
+                        Reserve.CancelReservation(currentUser);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input. Please select a valid option.");
+                        break;
+                }
+                Console.ReadKey();
+                Console.Clear();
+                return true;
+            case "2":
+                Menu.Display_menu();
+                Console.ReadKey();
+                Console.Clear();
+                return true;
+            case "3":
+                Information.ContactInfo();
+                Console.ReadKey();
+                Console.Clear();
+                return true;
+            case "4":
+                Console.WriteLine("Log out successful.");
+                Console.ReadKey();
+                Console.Clear();
+                return false;
+            case "5":
+                Console.WriteLine("Exiting the app. Goodbye!");
+                Environment.Exit(0);
+                return true;
+            case "6":
+                if (!currentUser.IsAdmin && !currentUser.IsSuperAdmin)
+                {
+                    Order order = new Order();
+                    order.PlaceOrder(currentUser);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please select a valid option.");
+                }
+                return true;
+            default:
+                Console.WriteLine("Invalid input. Please select a valid option.");
+                return true;
+        }
     }
 }
