0c467f1bfea9c46e2360375120529f1ca9e31811
Aidan <143912273+AidanYappington@users.noreply.github.com>
2024-01-10 12:29:32 +0100

    Update Program.cs
    added 7th option to the list

--- Restaurant/Program.cs
+++ Restaurant/Program.cs
@@ -15,26 +15,9 @@ public static class Program
 
     public static void Main()
     {
-        /*string filePath = "C:\\Users\\jerre\\OneDrive\\Bureaublad\\projectbb\\projectbb\\User_info.json";
-
-        // Read existing user data from the file
-        List<User> users = JsonConvert.DeserializeObject<List<User>>(File.ReadAllText(filePath), new JsonSerializerSettings
-        {
-            TypeNameHandling = TypeNameHandling.All
-        });
-
-        // Add the hardcoded superadmin to the list
-        SuperAdmin superAdmin = new SuperAdmin("superadmin", "superadmin@example.com", "superadmin");
-        users.Add(superAdmin);
-
-        // Serialize the updated list of users with type information
-        string jsonString = JsonConvert.SerializeObject(users, Formatting.Indented, new JsonSerializerSettings
-        {
-            TypeNameHandling = TypeNameHandling.All
-        });
-
-        // Write the updated user data back to the file
-        File.WriteAllText(filePath, jsonString);*/
+        /*
+            copy-paste hardocde here..
+        */
         while (true)
         {
             Console.WriteLine("+--------------------------------+");
@@ -62,13 +45,13 @@ public static class Program
                         if (currentUser is Admin)
                             Console.WriteLine("Please select an option (1/2/3/4/5/6):");
                         else
-                            Console.WriteLine("Please select an option (1/2/3/4/5):");
+                            Console.WriteLine("Please select an option (1/2/3/4/5/6/7):");
                         if (currentUser.UserInput(currentUser, tables) is false)
                             Loggedin = false;
                     }
                     break;
                 case "2":
-                    Menu.Display_menu();
+                    Menu.Display_menu(Menu.ActiveFoodMenu, Menu.ActiveDrinkMenu);
                     Console.ReadKey();
                     Console.Clear();
                     break;
@@ -90,18 +73,18 @@ public static class Program
 
     public static void Login_or_Signup()
     {
-        Console.WriteLine("Do you want to LOGIN(1) or Sign up(2) ");
-        string user_answer = Console.ReadLine().ToLower();
-
         while (true)
         {
+            Console.WriteLine("Do you want to LOGIN(1) or Sign up(2) ");
+            string user_answer = Console.ReadLine().ToLower();
+
             if (user_answer == "1")
             {
                 Login userLogin = new Login();
                 currentUser = userLogin.PromptForLogin();
                 if (currentUser == null)
                 {
-                    Console.WriteLine("Invalid login. Please try again or sign up.");
+                    Console.WriteLine("Please try again or sign up.");
                 }
                 else
                 {
@@ -113,16 +96,8 @@ public static class Program
             else if (user_answer == "2")
             {
                 Signup usersignup = new Signup();
-                currentUser = usersignup.SignUp(null, null,null,false);
-            /*    Console.WriteLine("Please enter your name: ");
-                string name = Console.ReadLine();
-                Console.WriteLine("Please enter your email: ");
-                string email = Console.ReadLine();
-                Console.WriteLine("Please enter your password: (At least 8 characters)");
-                string password = Console.ReadLine();
+                currentUser = usersignup.SignUp(null, null, null, false);
 
-                // Call the SignUp method to validate and save user information
-                currentUser = usersignup.SignUp(name, email, password, false);*/
                 if (currentUser == null)
                 {
                     Console.WriteLine("Invalid signup. Please try again.");
@@ -137,14 +112,13 @@ public static class Program
             {
                 Console.WriteLine("Invalid choice. Please enter either 1 or 2.");
             }
-
-            Console.WriteLine("Do you want to LOGIN(1) or Sign up(2) ");
-            user_answer = Console.ReadLine().ToLower(); // Get the user's choice again.
         }
 
         Console.ReadKey();
         Console.Clear();
     }
+
+
     //CODE BELOW TO HARDCODE A SUPERADMIN
     //==================================
 
