2b3c3ac611f3b7cf88d75b18e339caca61619f47
Aidan Lewi <143912273+AidanYappington@users.noreply.github.com>
2024-01-12 21:52:33 +0100

    Updated paths Menu.cs

--- Restaurant/Menu.cs
+++ Restaurant/Menu.cs
@@ -2,26 +2,20 @@ using Newtonsoft.Json;
 using System.IO;
 public static class Menu
 {
-    static private List<Food> List_of_Foods = new List<Food>();
-    static private List<Drink> List_of_Drinks = new List<Drink>();
+    public static string ActiveFoodMenu => Food_Menu_A;
+    public static string ActiveDrinkMenu => Drink_Menu_A;
 
-    private static string _activeFoodMenu = @"C:\Users\aidan\OneDrive\Documenten\c# docs\RestaurantAltaaf\RestaurantAltaaf\Menu_Food.json"; // Default value
-    private static string _activeDrinkMenu = @"C:\Users\aidan\OneDrive\Documenten\c# docs\RestaurantAltaaf\RestaurantAltaaf\Menu_Drink.json"; // Default value
+    public static string FutureFood => Food_Menu_B;
+    public static string FutureDrink => Drink_Menu_B;
 
-    public static string ActiveFoodMenu
-    {
-        get => _activeFoodMenu;
-        set => _activeFoodMenu = value;
-    }
+    // Paths to the menus
+    public static string Food_Menu_A = @"../../../Menu_Food.json";
+    public static string Drink_Menu_A = @"../../../Menu_Drink.json";
 
-    public static string ActiveDrinkMenu
-    {
-        get => _activeDrinkMenu;
-        set => _activeDrinkMenu = value;
-    }
+    public static string Food_Menu_B = @"../../../Menu_Food_B.json";
+    public static string Drink_Menu_B = @"../../../Menu_Drink_B.json";
 
-    public static string FutureFood { get; set; }
-    public static string FutureDrink { get; set; }
+
 
     public static List<string> FoodMenus = new List<string>
     {
@@ -70,55 +64,118 @@ public static class Menu
     }
 
 
-    public static void Add_Item_Menu()
+    public static void Add_Item_Menu() // admin method
     {
-        Console.WriteLine("What would you like to add?\n1: Food\n2: Drink");
-        string userInput = Console.ReadLine();
-        switch (userInput)
+        bool validInput = false;
+
+        do
         {
-            case "1":
-                Food new_dish = (Food)Item_Setup(userInput);
-                Console.WriteLine("To which menu would you like to add this item (1/2)");
-                Console.WriteLine("1: Active Food Menu\n2: Future Food Menu");
+            Console.WriteLine("What would you like to add?\n1: Food\n2: Drink");
+            string userInput = Console.ReadLine();
 
-                string inputmenu = Console.ReadLine();
-                if (inputmenu == "1")
-                {
+            switch (userInput)
+            {
+                case "1":
+                    Food new_dish = (Food)Item_Setup(userInput);
                     AddMenuItem(new_dish, ActiveFoodMenu);
-                }
-                else
-                {
-                    AddMenuItem(new_dish, FutureFood);
-                }
-                Console.WriteLine("The item has been added to the menu.");
-
-                break;
-            case "2":
-                Drink new_drink = (Drink)Item_Setup(userInput);
-                Console.WriteLine("To which menu would you like to add this item (1/2)?");
-                Console.WriteLine("1: Active Food Menu\n2: Future Food Menu");
-                string inputdrink = Console.ReadLine();
-
-                if (inputdrink == "1")
-                {
+                    Console.WriteLine("The item has been added to the menu.");
+                    validInput = true;
+                    break;
+                case "2":
+                    Drink new_drink = (Drink)Item_Setup(userInput);
                     AddMenuItem(new_drink, ActiveDrinkMenu);
-                }
-                else
-                {
-                    AddMenuItem(new_drink, FutureDrink);
-                }
-
-                Console.WriteLine("The item has been added to the menu.");
-                break;
-        }
+                    Console.WriteLine("The item has been added to the menu.");
+                    validInput = true;
+                    break;
+                default:
+                    Console.WriteLine("Invalid input. Please enter 1 for Food or 2 for Drink.");
+                    break;
+            }
+        } while (!validInput);
     }
+
+    public static void Add_Item_Menu(string superadmin) // super admin method
+    {
+        do
+        {
+            Console.WriteLine("What would you like to add?\n1: Food\n2: Drink");
+            string userInput = Console.ReadLine();
+
+            switch (userInput)
+            {
+                case "1":
+                    Food new_dish = (Food)Item_Setup(userInput);
+                    Console.WriteLine("To which menu would you like to add this item (1/2)");
+                    Console.WriteLine("1: Active Food Menu\n2: Unactive Food Menu");
+
+                    string inputmenu = Console.ReadLine();
+                    if (inputmenu == "1")
+                    {
+                        AddMenuItem(new_dish, ActiveFoodMenu);
+                    }
+                    else if (inputmenu == "2")
+                    {
+                        AddMenuItem(new_dish, FutureFood);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input. Please enter 1 or 2.");
+                        continue; // Go back to the beginning of the loop
+                    }
+                    Console.WriteLine("The item has been added to the menu.");
+                    break;
+                case "2":
+                    Drink new_drink = (Drink)Item_Setup(userInput);
+                    Console.WriteLine("To which menu would you like to add this item (1/2)?");
+                    Console.WriteLine("1: Active Drink Menu\n2: Unactive Drink Menu");
+                    string inputdrink = Console.ReadLine();
+
+                    if (inputdrink == "1")
+                    {
+                        AddMenuItem(new_drink, ActiveDrinkMenu);
+                    }
+                    else if (inputdrink == "2")
+                    {
+                        AddMenuItem(new_drink, FutureDrink);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input. Please enter 1 or 2.");
+                        continue; // Go back to the beginning of the loop
+                    }
+
+                    Console.WriteLine("The item has been added to the menu.");
+                    break;
+                default:
+                    Console.WriteLine("Invalid input. Please enter 1 or 2.");
+                    break;
+            }
+
+            break; // Exit the loop if the input is processed successfully
+
+        } while (true);
+    }
+
     public static object Item_Setup(string F_D)
     {
 
         Console.WriteLine("What is the name of the item?");
         string Item_name = Console.ReadLine();
-        Console.WriteLine("What is the price of the item?");
-        double Item_price = Convert.ToDouble(Console.ReadLine());
+        double Item_price;
+        while (true)
+        {
+            Console.WriteLine("What is the price of the item?");
+            string priceInput = Console.ReadLine();
+
+            if (double.TryParse(priceInput, out Item_price))
+            {
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid price.");
+            }
+        }
         if (F_D == "1")
         {
             bool Dish_vegan = Vegan_Setup(Item_name, Item_price);
@@ -149,7 +206,7 @@ public static class Menu
         return false;
     }
 
-    public static void Remove_Item_Menu()
+    public static void Remove_Item_Menu(string superadmin) // super admin method
     {
         Console.WriteLine("What would you like to remove?\n1: Food item\n2: Drink item");
         string userInput = Console.ReadLine();
@@ -204,7 +261,52 @@ public static class Menu
         }
     }
 
-    static public void Display_menu(string foodMenuPath, string drinkMenuPath)
+    public static void Remove_Item_Menu() // admin method
+    {
+        Console.WriteLine("What would you like to remove?\n1: Food item\n2: Drink item");
+
+        string userInput = Console.ReadLine();
+
+        bool validInput = false;
+
+        while (!validInput)
+        {
+            switch (userInput)
+            {
+                case "1":
+                    string inputFood = Console.ReadLine();
+                    if (inputFood == "1")
+                    {
+                        Delete_Item<Food>(ActiveFoodMenu);
+                        validInput = true;
+                    }
+                    
+                    else
+                    {
+                        Console.WriteLine("Invalid input. Please enter 1 or 2.");
+                    }
+                    break;
+                case "2":
+                    string inputDrink = Console.ReadLine();
+                    if (inputDrink == "1")
+                    {
+                        Delete_Item<Drink>(ActiveDrinkMenu);
+                        validInput = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input. Please enter 1 or 2.");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Invalid selection. Please enter 1 or 2.");
+                    userInput = Console.ReadLine();
+                    break;
+            }
+        }
+    }
+
+    public static void Display_menu(string foodMenuPath, string drinkMenuPath)
     {
         if (foodMenuPath == null || drinkMenuPath == null)
         {
@@ -279,12 +381,9 @@ public static class Menu
 
     static public void AddMenuItem<T>(T item, string menuPath)
     {
-        // Deserialize the existing data from the JSON file
         string json = File.ReadAllText(menuPath);
         List<T> existingItems = JsonConvert.DeserializeObject<List<T>>(json);
-        // Add the new item to the existing list
         existingItems.Add(item);
-        // Serialize and write the updated list back to the JSON file
         string updatedJson = JsonConvert.SerializeObject(existingItems, Formatting.Indented);
         File.WriteAllText(menuPath, updatedJson);
     }
@@ -294,21 +393,18 @@ public static class Menu
         Console.WriteLine("Name of the item that you want to delete: ");
         string Item_name = Console.ReadLine();
 
-
-        // Deserialize the existing data from the JSON file
         string json = File.ReadAllText(fm3nupath);
         List<T> existingItems = JsonConvert.DeserializeObject<List<T>>(json);
 
         if (typeof(T) == typeof(Food) || typeof(T) == typeof(Drink))
         {
-            // Find and remove the food item by its name
+            // vind en verwijder item
             T selectedItem = existingItems.FirstOrDefault(item => item.Name.Equals(Item_name, StringComparison.OrdinalIgnoreCase));
 
             if (selectedItem != null)
             {
                 existingItems.Remove(selectedItem);
 
-                // Serialize and write the updated list back to the JSON file
                 string updatedJson = JsonConvert.SerializeObject(existingItems, Formatting.Indented);
                 File.WriteAllText(fm3nupath, updatedJson);
 
@@ -334,54 +430,9 @@ public static class Menu
         return selectedItem;
     }
 
-    public static void Create_new_menu()
-    {
-        // Basis items
-        List<Food> foods = new List<Food>
-        {
-            new Food("Burger", 9.99, false),
-            new Food("Special burger", 11.99, false),
-            new Food("apple salad", 6.99, true),
-        };
+    
 
-
-        Console.WriteLine("Name of the new food Menu: ");
-        string foodFileName = Console.ReadLine();
-
-        Console.WriteLine("Name of the new drink Menu: ");
-        string drinkFileName = Console.ReadLine();
-
-
-        string foodJson = JsonConvert.SerializeObject(foods, Formatting.Indented);
-        File.WriteAllText($"C:\\Users\\aidan\\OneDrive\\Documenten\\c# docs\\RestaurantAltaaf\\RestaurantAltaaf\\{foodFileName}", foodJson);
-        FutureFood = @"C:\\Users\\aidan\\OneDrive\\Documenten\\c# docs\\RestaurantAltaaf\\RestaurantAltaaf\\" + foodFileName+".json";
-        // Basisitems
-        List<Drink> drinks = new List<Drink>
-        {
-            new Drink("Water", 2.0),
-            new Drink("Cola", 3.5),
-            new Drink("Python juice", 4.99),
-        };
-
-        string drinkJson = JsonConvert.SerializeObject(drinks, Formatting.Indented);
-        File.WriteAllText($"C:\\Users\\aidan\\OneDrive\\Documenten\\c# docs\\RestaurantAltaaf\\RestaurantAltaaf\\{drinkFileName}", drinkJson);
-        FutureDrink = @"C:\\Users\\aidan\\OneDrive\\Documenten\\c# docs\\RestaurantAltaaf\\RestaurantAltaaf\\" + drinkFileName +".json";
-        Console.WriteLine(
- @"+--------------------------------+
-|                                |
-| Succesfully created the menu   |
-|                                |
-| Press Enter button to return.  |
-+--------------------------------+");
-
-        ManageList(FoodMenus, FutureFood);
-        ManageList(DrinkMenus, FutureDrink);
-        Console.ReadKey();
-
-
-    }
-
-    public static void Activate_Menus()
+    public static void Switch_menus()
     {
         Console.WriteLine(
  @"+--------------------------------+
@@ -390,8 +441,19 @@ public static class Menu
 |                                |
 | Press Enter button to return.  |
 +--------------------------------+");
-        ActiveFoodMenu = FutureFood;
-        ActiveDrinkMenu = FutureDrink;
+
+
+        // Swap the active and future food menus paths
+        string tempFoodPath = Food_Menu_A;
+        Food_Menu_A = Food_Menu_B;
+        Food_Menu_B = tempFoodPath;
+
+        // Swap the active and future drink menus paths
+        string tempDrinkPath = Drink_Menu_A;
+        Drink_Menu_A = Drink_Menu_B;
+        Drink_Menu_B = tempDrinkPath;
+
+
         Console.ReadKey();
     }
 
