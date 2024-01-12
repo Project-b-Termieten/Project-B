cc74ab956a8e42ac2fb777b577a13de6ea18854d
Aidan Lewi <143912273+AidanYappington@users.noreply.github.com>
2024-01-12 12:53:21 +0100

    Update Program.cs
    bugfix

--- Restaurant/Program.cs
+++ Restaurant/Program.cs
@@ -15,23 +15,57 @@ public static class Program
 
     public static void Main()
     {
-        /*
-            copy-paste hardocde here..
-        */
+        /*string filePath = "C:\\Users\\jerre\\OneDrive\\Bureaublad\\projectbb\\projectbb\\User_info.json";
+
+        // Read existing user data from the file
+        List<User> users = JsonConvert.DeserializeObject<List<User>>(File.ReadAllText(filePath), new JsonSerializerSettings
+        {
+            TypeNameHandling = TypeNameHandling.All
+        });
+
+        // Add the hardcoded superadmin to the list
+        SuperAdmin superAdmin = new SuperAdmin("superadmin", "superadmin@example.com", "superadmin");
+        users.Add(superAdmin);
+
+        // Serialize the updated list of users with type information
+        string jsonString = JsonConvert.SerializeObject(users, Formatting.Indented, new JsonSerializerSettings
+        {
+            TypeNameHandling = TypeNameHandling.All
+        });
+
+        // Write the updated user data back to the file
+        File.WriteAllText(filePath, jsonString);*/
+        
+
+        //START VAN PROGRAMMA IS HIER \/
+        Startmenu();
+    }
+
+    public static void Startmenu()
+    {
         while (true)
         {
-            Console.WriteLine("+--------------------------------+");
-            Console.WriteLine("|                                |");
-            Console.WriteLine("|  Welcome to Jake’s restaurant! |");
-            Console.WriteLine("|                                |");
-            Console.WriteLine("+--------------------------------+");
-            Console.WriteLine("| Options:                       |");
-            Console.WriteLine("| 1. Login or Signup             |");
-            Console.WriteLine("| 2. Menu                        |");
-            Console.WriteLine("| 3. Restaurant Information      |");
-            Console.WriteLine("| 4. Exit                        |");
-            Console.WriteLine("+--------------------------------+");
-            Console.WriteLine("Please pick an option (1/2/3/4):  ");
+            //Console.BackgroundColor = ConsoleColor.White;
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(@"
++--------------------------------+
+|                                |
+|  Welcome to Jake’s restaurant! |
+|  'A Sharp Dining Experience'   |
+|                                |
++--------------------------------+");
+            Console.ResetColor();
+
+            Console.WriteLine(@"
++--------------------------------+
+| Please select an option:       |
+| 1. Login or Signup             |
+| 2. Menu                        |
+| 3. Restaurant Information      |
+| 4. Exit                        |
++--------------------------------+");
+
             string userinput = Console.ReadLine();
             switch (userinput)
             {
@@ -43,9 +77,9 @@ public static class Program
                     {
                         currentUser.UserMenu();
                         if (currentUser is Admin)
-                            Console.WriteLine("Please select an option (1/2/3/4/5/6):");
+                            Console.WriteLine("Please select an option: ");
                         else
-                            Console.WriteLine("Please select an option (1/2/3/4/5/6/7):");
+                            Console.WriteLine("Please select an option: ");
                         if (currentUser.UserInput(currentUser, tables) is false)
                             Loggedin = false;
                     }
@@ -53,6 +87,15 @@ public static class Program
                 case "2":
                     Menu.Display_menu(Menu.ActiveFoodMenu, Menu.ActiveDrinkMenu);
                     Console.ReadKey();
+                    if (Menu.FutureFood == null)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        Menu.Display_menu(Menu.FutureFood, Menu.FutureDrink);
+
+                    }
                     Console.Clear();
                     break;
                 case "3":
@@ -61,11 +104,21 @@ public static class Program
                     Console.Clear();
                     break;
                 case "4":
-                    Console.WriteLine("Exiting the app. Goodbye!");
+                    Console.WriteLine(@"
++--------------------------------------------------------+
+| Looking forward to see you at our restaurant, dag dag!! |
++--------------------------------------------------------+");
+
+
                     Environment.Exit(0);
                     break;
                 default:
-                    Console.WriteLine("Invalid input. Please select a valid option.");
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.Red;
+
+                    Console.WriteLine("Invalid input. Please choose a number between 1 and 4.");
+                    Console.ResetColor();
+
                     break;
             }
         }
@@ -75,7 +128,78 @@ public static class Program
     {
         while (true)
         {
-            Console.WriteLine("Do you want to LOGIN(1) or Sign up(2) ");
+            Console.WriteLine(@"
++--------------------------------+
+| Please select an option or pr- |
+| ess Enter to return.           |
+| 1. Login                       |
+| 2. Signup                      |
++--------------------------------+");
+
+            string user_answer = Console.ReadLine().ToLower();
+
+            switch (user_answer)
+            {
+                case "1":
+                    Login userLogin = new Login();
+                    currentUser = userLogin.PromptForLogin();
+                    if (currentUser == null)
+                    {
+                        Console.WriteLine("| Please try again or sign up.   |");
+                        Console.WriteLine("+--------------------------------+");
+                    }
+                    else
+                    {
+                        string email = currentUser.Email;
+                        Console.WriteLine("Logging in with email: " + email);
+                        return; // Exit the method if login is successful.
+                    }
+                    break;
+
+                case "2":
+                    Signup usersignup = new Signup();
+                    currentUser = usersignup.SignUp(null, null, null, false);
+
+                    if (currentUser == null)
+                    {
+                        Console.WriteLine("Invalid signup. Please try again.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("+----------------------------------------+");
+                        Console.WriteLine("| Your information has been saved!.      |");
+                        Console.WriteLine("+----------------------------------------+");
+                        Console.WriteLine(@"| Logging in with email: " + currentUser.Email.PadRight(15) + @" |
++----------------------------------------+");
+                        return; // Exit the method if signup is successful.
+                    }
+                    break;
+                case "":
+                    Console.Clear();
+                    Startmenu();
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid choice. Please enter either 1, 2 or press ENTER to return.");
+                    break;
+            }
+        }
+
+        Console.ReadKey();
+        Console.Clear();
+    }
+
+
+    /*public static void Login_or_Signup()
+    {
+        while (true)
+        {
+            Console.WriteLine(@"
++--------------------------------+
+| Please select an option:       |
+| 1. Login                       |
+| 2. Signup                      |
++--------------------------------+");
             string user_answer = Console.ReadLine().ToLower();
 
             if (user_answer == "1")
@@ -95,7 +219,8 @@ public static class Program
             }
             else if (user_answer == "2")
             {
-                currentUser = Signup.SignUp(null, null, null, false);
+                Signup usersignup = new Signup();
+                currentUser = usersignup.SignUp(null, null, null, false);
 
                 if (currentUser == null)
                 {
@@ -115,7 +240,7 @@ public static class Program
 
         Console.ReadKey();
         Console.Clear();
-    }
+    }*/
 
 
     //CODE BELOW TO HARDCODE A SUPERADMIN
@@ -141,4 +266,6 @@ string jsonString = JsonConvert.SerializeObject(users, Formatting.Indented, new 
 
 // Write the updated user data back to the file
 //File.WriteAllText(filePath, jsonString);*/
+
+    
 }
